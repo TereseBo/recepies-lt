5ee105ae2c76bbc7eeb753dc4b6f10ce1eaa6ce3
TereseBo~ <terese.boderus@yh.nackademin.se>
2023-02-09 09:03:30 +0100

    Added template for recepies and started functions for html-creation on recepie list

--- scripts/recept.js
+++ scripts/recept.js
@@ -1,21 +1,29 @@
 async function getRecipes() {
     let res = await fetch("http://localhost:3001/recepies");
-        const data = await res.json();
-        console.log(data);
-
-
-
-if (res.ok) {
-    console.log("GET successful");
-
-
+    const data = await res.json();
+    console.log(data);
+    if (res.ok) {
+        console.log("GET successful");
+        return data;
+    } else {
+        console.log("GET failed");
+        return null;
+    }
+}
+function makeRecipe(recipe) {
+    let template=document.getElementById("recipe-template").cloneNode(true);
+    console.log(template);
 
 }
+function makeRecipeList(data) {
+    data.forEach(element => {
+        makeRecipe(element);
+    });
 }
-
 
 
 document.addEventListener("DOMContentLoaded", () => {
-getRecipes();
+    let data=getRecipes();
+    makeRecipeList(data);
 
 })
