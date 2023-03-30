0216911d6fe6c86612e73baccb22ee899c51e243
TereseBo~ <terese.boderus@yh.nackademin.se>
2023-03-30 15:05:39 +0200

    Refactored registration function

--- client/scripts/reg.js
+++ client/scripts/reg.js
@@ -1,40 +1,33 @@
 
 
 function addIngredient(e) {//Adds an ingrddient field to the recipe form
-    const ingredient = document.getElementById("ingredient-field").cloneNode(true);
+    const ingredient = document.getElementById("ingredient-field-0").cloneNode(true);
+    const ingredientId = document.getElementById("ingredients-container").children.length;
+    ingredient.id = `ingredient-field-${ingredientId}`;
     let ingredientContainer = document.getElementById("ingredients-container");
     Array.from(ingredient.children).forEach(child => {child.value = ""});
     ingredientContainer.appendChild(ingredient);
 }
 async function registerRecipe(e) {//Sends the recipe to the server
     e.preventDefault();
-    const myFormData = new FormData(e.currentTarget);
+    const form=document.getElementById("register-recipe");
+
     const formDataObj = {};
-    formDataObj["ingredients"] = [];
-    formDataObj["amounts"] = [];
-    formDataObj["units"] = [];
-    formDataObj["category"] = [];
-    console.log(myFormData);
-    myFormData.forEach((value, key) => {
-        if(key === "ingredient"|| key === "unit" || key === "category"|| key === "amount"){
-            switch(key){
-                case "ingredient":
-            formDataObj["ingredients"].push(value);
-            break;
-            case "unit":
-            formDataObj["units"].push(value);
-            break;
-            case "category":
-            formDataObj["category"].push(value);
-            break;
-            case "amount":
-            formDataObj["amounts"].push(value);
-            break;
-            }}
-        else{        
-        formDataObj[key] = value
-        }});
-    let recipe = formDataObj;
+    formDataObj.name = form.querySelector("#name").value;
+    formDataObj.ingredients = [];
+    formDataObj.comment = form.querySelector("#comment").value;
+    const ingredients = document.getElementById("ingredients-container").children;
+   Array.from(ingredients).forEach(ingredient => {//Creates an ingredient object for each ingredient field
+        const ingredientObj = {};
+        console.log(ingredient);
+       console.log(ingredient.querySelector("#ingredient").value);
+        ingredientObj.name = ingredient.querySelector("#ingredient").value;
+        ingredientObj.amount = ingredient.querySelector("#amount").value;
+        ingredientObj.unit = ingredient.querySelector("#unit").value;
+        ingredientObj.category = ingredient.querySelector("#category").value;
+        formDataObj["ingredients"].push(ingredientObj);
+    });
+    const recipe = formDataObj;
     console.log(formDataObj);
 
     let res = await fetch("http://localhost:3001/recepies", {
@@ -47,10 +40,7 @@ async function registerRecipe(e) {//Sends the recipe to the server
     });
     if (res.ok) {
         console.log(res.status);
-
-
         e.target.reset();
-        // alert("Ditt recept har registrerats!");
     }
 }
 
